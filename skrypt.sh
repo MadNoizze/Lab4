2d60ad03468edb05e64d953334c0e039f51ea505
Amadeusz <abarylak40@gmail.com>
2025-05-17 20:52:55 +0200

    Zmiany nowe

--- skrypt.sh
+++ skrypt.sh
@@ -20,4 +20,18 @@ case $1 in
   *)
     echo "Nieznana opcja. Użyj --help lub -h, aby zobaczyć dostępne opcje."
     ;;
+  --error | -e)
+  count=${2:-100}
+  for ((i=1; i<=count; i++)); do
+    mkdir -p error$i
+    echo -e "Error folder: error$i\nPlik: error$i.txt\nData: $(date)" > error$i/error$i.txt
+  done
+  echo "$count folderów error zostało utworzonych."
+  ;;
+  --init)
+  git clone https://github.com/<TwojeKonto>/ZadanieSkrypt.git
+  export PATH=$PATH:$(pwd)/ZadanieSkrypt
+  echo "Repozytorium zostało sklonowane i PATH zaktualizowany."
+  ;;
+
 esac
